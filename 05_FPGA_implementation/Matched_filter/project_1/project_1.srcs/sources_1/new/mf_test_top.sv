79ce3461362e6187d0ce143575c2fdfe68c3667d
UCL-24014409 <danica.lindsay30@gmail.com>
2026-09-22 15:47:32 +0100

    [code] Creating mf top module

---
+++ 05_FPGA_implementation/Matched_filter/project_1/project_1.srcs/sources_1/new/mf_test_top.sv
@@ -0,0 +1,23 @@
+`timescale 1ns / 1ps
+
+
+module mf_test_top(input logic clk,
+                   input logic rst,
+                   output logic valid_out,
+                   output logic signed [17:0] filtered_Ix,
+                   output logic signed [17:0] filtered_Qx,
+                   output logic signed [17:0] filtered_Iy,
+                   output logic signed [17:0] filtered_Qy);
+       
+       
+       
+       //internal signals
+       
+       //counter logic 
+       
+       //ROM logic 
+       
+       //control and unpacking logic 
+       
+       //filter instantiation 
+endmodule
